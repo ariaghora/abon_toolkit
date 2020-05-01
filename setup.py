ebb4836181647662caa7055488f842a5cdf84d98
Aria Ghora Prabono <hello@ghora.net>
2020-05-01 15:06:29 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='torch_kernel',
+    name='abon_toolkit',
     version='1.0.0',
     description=('Kernel functions for pytorch'),
     author='Aria Ghora Prabono',
     author_email='hello@ghora.net',
     url='https://github.com/ariaghora/torch_kernel',
     license='MIT',
-    packages=['torch_kernel'],
+    packages=['abon_toolkit'],
     include_package_data=True,
     classifiers=[
         'Intended Audience :: Science/Research',
